0224190e13c8b50a3ac588f66fbc95a3cd65b8fe
Chris Siannas <xrsia@otenet.gr>
2022-02-12 08:08:04 +0200

    Finish

--- range-channels.go
+++ range-channels.go
@@ -1,12 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 	//make a channel
-
+	ch := make(chan int)
 	/*start go function
 	send into the channel six numbers
 	close the channel
 	*/
-
+	go func() {
+		for i := 0; i < 6; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
 	//use range to receive the contents of the channel
+	for j := range ch {
+		fmt.Println(j)
+	}
 }
