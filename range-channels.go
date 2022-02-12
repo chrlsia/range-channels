804c290eb1d0e4ae1eab4811ed2ef482f0c52151
Chris Siannas <xrsia@otenet.gr>
2022-02-12 08:06:12 +0200

    Outline of the program

--- range-channels.go
+++ range-channels.go
@@ -1,5 +1,12 @@
 package main
 
 func main() {
+	//make a channel
 
+	/*start go function
+	send into the channel six numbers
+	close the channel
+	*/
+
+	//use range to receive the contents of the channel
 }
